448b531018f05e9b437246d3c6f92457c799703a
Norda24 <ghostnorda24@gmail.com>
2026-04-27 15:59:30 +0300

    .

--- S13/models.py
+++ S13/models.py
@@ -1,7 +1,6 @@
 from peewee import *
 import datetime
 
-# Подключение к базе данных SQLite
 db = SqliteDatabase('work_programs.db')
 
 class BaseModel(Model):
@@ -9,38 +8,30 @@ class BaseModel(Model):
         database = db
 
 class WorkProgram(BaseModel):
-    """Сущность: Рабочая программа"""
-    title = CharField(null=False)  # Название программы
-    file_path = CharField(null=False)  # Путь к файлу программы
+    title = CharField(null=False)
+    file_path = CharField(null=False)
     version = CharField(null=False, default="1.0")
     created_at = DateTimeField(default=datetime.datetime.now, null=False)
 
 class Specialty(BaseModel):
-    """Сущность: Специальность (из смежного сервиса)"""
-    code = CharField(unique=True, null=False) # Например, '09.02.07'
+    code = CharField(unique=True, null=False)
     name = CharField(null=False)
 
 class ProgramAssignment(BaseModel):
-    """Транзитивная таблица для связи Многие-ко-многим"""
     program = ForeignKeyField(WorkProgram, backref='assignments', null=False)
     specialty = ForeignKeyField(Specialty, backref='programs', null=False)
-    discipline_id = IntegerField(null=False) # ID дисциплины
+    discipline_id = IntegerField(null=False) # То самое поле
 
     class Meta:
-        # Ограничение: нельзя привязать одну и ту же программу к одной специальности дважды
         indexes = (
             (('program', 'specialty', 'discipline_id'), True),
         )
 
 def init_db():
-    """Функция инициализации базы данных"""
     db.connect()
     db.create_tables([WorkProgram, Specialty, ProgramAssignment])
-    print("Сервис рабочих программ: БД инициализирована.")
+    print("БД инициализирована.")
 
 if __name__ == "__main__":
     init_db()
-
-
-
 
